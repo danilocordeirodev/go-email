07bb1f058cffbe77a4b4cd0085c8653314884d34
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2023-10-14 23:48:41 -0300

    increase tests

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -2,11 +2,12 @@ package campaign
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewCampaign(t *testing.T) {
+func Test_NewCampaign_CreateCampaign(t *testing.T) {
 	assert := assert.New(t)
 	name := "Campaign X"
 	content := "Body"
@@ -14,9 +15,24 @@ func TestNewCampaign(t *testing.T) {
 
 	campaign := NewCampaign(name, content, contacts)
 
-	assert.Equal(campaign.ID, "1")
+	assert.NotNil(campaign.ID)
 	assert.Equal(campaign.Name, name)
 	assert.Equal(campaign.Content, content)
 	assert.Equal(len(campaign.Contacts), len(contacts))
+	assert.NotNil(campaign.CreatedOn)
+
+}
+
+func Test_NewCampaign_CreatedOnIsNotNil(t *testing.T) {
+	assert := assert.New(t)
+	name := "Campaign X"
+	content := "Body"
+	contacts := []string{"email@e.com", "email2@e.com"}
+
+	campaign := NewCampaign(name, content, contacts)
+	now := time.Now().Add(-time.Minute)
+	
+	assert.Greater(campaign.CreatedOn, now)
+
 
 }
